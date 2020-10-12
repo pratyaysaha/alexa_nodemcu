0772738b05044c565fb7f800367acf38a3d3f313
pratyaysaha <pratyaysaha@outlook.com>
2020-10-12 22:26:22 +0530

    Create ALEXA_SWITCH___LDR.ino

---
+++ ALEXA_SWITCH___LDR/ALEXA_SWITCH___LDR.ino
@@ -0,0 +1,202 @@
+/*
+ Version 0.4 - April 26 2019
+*/ 
+
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <WebSocketsClient.h> //  https://github.com/kakopappa/sinric/wiki/How-to-add-dependency-libraries
+#include <ArduinoJson.h> // https://github.com/kakopappa/sinric/wiki/How-to-add-dependency-libraries (use the correct version)
+#include <StreamString.h>
+
+int device_1=5;
+int device_2=4;
+int device_3=0;
+int device_4=2;
+int ldrval=0;
+
+ESP8266WiFiMulti WiFiMulti;
+WebSocketsClient webSocket;
+WiFiClient client;
+
+#define MyApiKey "d31d3abf-2df8-4f0d-9865-46fbb97aeb58" // TODO: Change to your sinric API Key. Your API Key is displayed on sinric.com dashboard
+#define MySSID "pratyay" // TODO: Change to your Wifi network SSID
+#define MyWifiPassword "26324926" // TODO: Change to your Wifi network password
+
+#define HEARTBEAT_INTERVAL 300000 // 5 Minutes 
+
+uint64_t heartbeatTimestamp = 0;
+bool isConnected = false;
+
+
+// deviceId is the ID assgined to your smart-home-device in sinric.com dashboard. Copy it from dashboard and paste it here
+
+void turnOn(String deviceId) {
+  if (deviceId == "5cf3c729262ff32f82cf973c") // Device ID of first device
+  {  
+    Serial.print("Turn on device id: ");
+    Serial.println(deviceId);
+    digitalWrite(device_1,LOW);
+  } 
+  else if (deviceId == "5cf3c73e262ff32f82cf973e") // Device ID of second device
+  { 
+    Serial.print("Turn on device id: ");
+    Serial.println(deviceId);
+    digitalWrite(device_2,LOW);
+  }
+    else if (deviceId == "5cf3c758262ff32f82cf9741") // Device ID of third device
+  { 
+    Serial.print("Turn on device id: ");
+    Serial.println(deviceId);
+    digitalWrite(device_3,LOW);
+  }
+    else if (deviceId == "5cf3c769262ff32f82cf9743") // Device ID of fourth device
+  { 
+    Serial.print("Turn on device id: ");
+    Serial.println(deviceId);
+    digitalWrite(device_4,LOW);
+    Serial.println(ldrval);
+  }
+  else {
+    Serial.print("Turn on for unknown device id: ");
+    Serial.println(deviceId);    
+  }     
+}
+
+void turnOff(String deviceId) {
+   if (deviceId == "5cf3c729262ff32f82cf973c") // Device ID of first device
+   {  
+     Serial.print("Turn off Device ID: ");
+     Serial.println(deviceId);
+     digitalWrite(device_1,HIGH);
+   }
+   else if (deviceId == "5cf3c73e262ff32f82cf973e") // Device ID of second device
+   { 
+     Serial.print("Turn off Device ID: ");
+     Serial.println(deviceId);
+     digitalWrite(device_2,HIGH);
+  }
+   else if (deviceId == "5cf3c758262ff32f82cf9741") // Device ID of third device
+   { 
+     Serial.print("Turn off Device ID: ");
+     Serial.println(deviceId);
+     digitalWrite(device_3,HIGH);
+  }
+   else if (deviceId == "5cf3c769262ff32f82cf9743") // Device ID of fourth device
+   { 
+     Serial.print("Turn off Device ID: ");
+     Serial.println(deviceId);
+     
+     digitalWrite(device_4,HIGH);
+     
+  }
+  else {
+     Serial.print("Turn off for unknown device id: ");
+     Serial.println(deviceId);    
+  }
+}
+
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
+  switch(type) {
+    case WStype_DISCONNECTED:
+      isConnected = false;    
+      Serial.printf("[WSc] Webservice disconnected from sinric.com!\n");
+      break;
+    case WStype_CONNECTED: {
+      isConnected = true;
+      Serial.printf("[WSc] Service connected to sinric.com at url: %s\n", payload);
+      Serial.printf("Waiting for commands from sinric.com ...\n");        
+      }
+      break;
+    case WStype_TEXT: {
+        Serial.printf("[WSc] get text: %s\n", payload);
+        // Example payloads
+
+        // For Switch or Light device types
+        // {"deviceId": xxxx, "action": "setPowerState", value: "ON"} // https://developer.amazon.com/docs/device-apis/alexa-powercontroller.html
+
+        // For Light device type
+        // Look at the light example in github
+          
+        DynamicJsonBuffer jsonBuffer;
+        JsonObject& json = jsonBuffer.parseObject((char*)payload); 
+        String deviceId = json ["deviceId"];     
+        String action = json ["action"];
+        
+        if(action == "setPowerState") { // Switch or Light
+            String value = json ["value"];
+            if(value == "ON") {
+                turnOn(deviceId);
+            } else {
+                turnOff(deviceId);
+            }
+        }
+        else if (action == "SetTargetTemperature") {
+            String deviceId = json ["deviceId"];     
+            String action = json ["action"];
+            String value = json ["value"];
+        }
+        else if (action == "test") {
+            Serial.println("[WSc] received test command from sinric.com");
+        }
+      }
+      break;
+    case WStype_BIN:
+      Serial.printf("[WSc] get binary length: %u\n", length);
+      break;
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(device_1,OUTPUT);
+  pinMode(device_2,OUTPUT);
+  pinMode(device_3,OUTPUT);
+  pinMode(device_4,OUTPUT);
+ 
+  
+  WiFiMulti.addAP(MySSID, MyWifiPassword);
+  Serial.println();
+  Serial.print("Connecting to Wifi: ");
+  Serial.println(MySSID);  
+
+  // Waiting for Wifi connect
+  while(WiFiMulti.run() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  if(WiFiMulti.run() == WL_CONNECTED) {
+    Serial.println("");
+    Serial.print("WiFi connected. ");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+  }
+
+  // server address, port and URL
+  webSocket.begin("iot.sinric.com", 80, "/");
+
+  // event handler
+  webSocket.onEvent(webSocketEvent);
+  webSocket.setAuthorization("apikey", MyApiKey);
+  
+  // try again every 5000ms if connection has failed
+  webSocket.setReconnectInterval(5000);   // If you see 'class WebSocketsClient' has no member named 'setReconnectInterval' error update arduinoWebSockets
+}
+
+void loop() {
+  webSocket.loop();
+  
+  if(isConnected) {
+      uint64_t now = millis();
+      
+      // Send heartbeat in order to avoid disconnections during ISP resetting IPs over night. Thanks @MacSass
+      if((now - heartbeatTimestamp) > HEARTBEAT_INTERVAL) {
+          heartbeatTimestamp = now;
+          webSocket.sendTXT("H");          
+      }
+  } 
+   ldrval=analogRead(A0);
+    
+}
+
+// If you want a push button: https://github.com/kakopappa/sinric/blob/master/arduino_examples/switch_with_push_button.ino  
